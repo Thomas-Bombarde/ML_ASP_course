4d8b98611099df1960c0686417216c920f237990
Tom Bombarde <thomas.bombarde@gmail.com>
2022-08-19 10:41:00 +0200

    cubic age

--- Pycharm_intro.py
+++ Pycharm_intro.py
@@ -3,4 +3,6 @@ import pandas as pd
 
 FNAME = "http://www.stat.ucla.edu/~rgould/datasets/twins.dat"
 
-df = pd.read_csv(FNAME, sep = "\t", na_values="?")
+df = pd.read_csv(FNAME, delimiter = "\t", na_values="?")
+
+df["AGECB"] = df["AGE"]**3
